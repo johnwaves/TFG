a28f248b806a48e0e4d80aef6c0054f1ca995733
Mihnea Vidican <jvidican103@gmail.com>
2024-11-10 23:18:44 +0100

    :art: add: incluye tarjetas pacientes #37

--- frontend/src/components/ui/Card.jsx
+++ frontend/src/components/ui/Card.jsx
@@ -310,6 +310,73 @@ const cardContent = {
     button: "Registrar",
   },
 
+  mistratamientos: {
+    svg: (
+      <svg
+        width="200"
+        height="100"
+        viewBox="0 0 200 100"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect
+          x="1"
+          y="1"
+          width="198"
+          height="98"
+          rx="15"
+          ry="15"
+          fill="none"
+          stroke="#000"
+          strokeWidth="2"
+        />
+
+        {Array.from({ length: 2 }).map((_, rowIndex) =>
+          Array.from({ length: 5 }).map((_, colIndex) => (
+            <ellipse
+              key={`${rowIndex}-${colIndex}`}
+              cx={30 + colIndex * 35}
+              cy={25 + rowIndex * 50}
+              rx="12"
+              ry="8"
+              fill="none"
+              stroke="#d3d3d3"
+              strokeWidth="1"
+            />
+          ))
+        )}
+      </svg>
+
+
+    ),
+    title: "Mis tratamientos",
+    description: "Ver todos mis tratamientos",
+    link: "/pacientes/tratamientos",
+    button: "Explorar",
+  },
+
+  registro: {
+    svg: (
+      <svg 
+        xmlns="http://www.w3.org/2000/svg" 
+        fill="none" 
+        viewBox="0 0 24 24" 
+        stroke-width="1" 
+        stroke="currentColor" 
+        class="size-24"
+      >
+        <path 
+          stroke-linecap="round" 
+          stroke-linejoin="round" 
+          d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75" />
+      </svg>
+
+    ),
+    title: "Registrar cumplimiento",
+    description: "Añadir un registro de cumplimiento para un tratamiento activo",
+    link: "/tratamientos/cumplimiento",
+    button: "Añadir",
+  },
+
   default: {
     svg: (
       <svg
