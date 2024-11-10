9633d1ce45797eeafbf56d3b1f7638bfd0ffdaf7
Mihnea Vidican <jvidican103@gmail.com>
2024-11-10 17:11:56 +0100

    :art: add: interfaz para tratamientos #36

--- frontend/src/components/ui/Card.jsx
+++ frontend/src/components/ui/Card.jsx
@@ -137,6 +137,179 @@ const cardContent = {
     button: "Crear",
   },
 
+  pacientes: {
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1}
+        stroke="currentColor"
+        className="size-24"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
+        />
+      </svg>
+    ),
+    title: "Pacientes",
+    description: "Añadir, modificar y eliminar pacientes de la farmacia",
+    link: "/pacientes",
+    button: "Explorar",
+  },
+
+  addpaciente: {
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1}
+        stroke="currentColor"
+        className="size-24"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+        />
+      </svg>
+    ),
+    title: "Crear paciente",
+    description: "Añadir un paciente y crear su ficha médica",
+    link: "/pacientes/crear",
+    button: "Añadir",
+  },
+
+  editpaciente: {
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1}
+        stroke="currentColor"
+        className="size-24"
+      >
+
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round" d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Zm6-10.125a1.875 1.875 0 1 1-3.75 0 1.875 1.875 0 0 1 3.75 0Zm1.294 6.336a6.721 6.721 0 0 1-3.17.789 6.721 6.721 0 0 1-3.168-.789 3.376 3.376 0 0 1 6.338 0Z" />
+      </svg>
+
+    ),
+    title: "Gestionar pacientes",
+    description: "Consultar, modificar y eliminar pacientes",
+    link: "/pacientes/listadopacientes",
+    button: "Gestionar",
+  },
+
+  tratamientos: {
+    svg: (
+      <svg
+        width="200"
+        height="100"
+        viewBox="0 0 200 100"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect
+          x="1"
+          y="1"
+          width="198"
+          height="98"
+          rx="15"
+          ry="15"
+          fill="none"
+          stroke="#000"
+          strokeWidth="2"
+        />
+
+        {Array.from({ length: 2 }).map((_, rowIndex) =>
+          Array.from({ length: 5 }).map((_, colIndex) => (
+            <ellipse
+              key={`${rowIndex}-${colIndex}`}
+              cx={30 + colIndex * 35}
+              cy={25 + rowIndex * 50}
+              rx="12"
+              ry="8"
+              fill="none"
+              stroke="#d3d3d3"
+              strokeWidth="1"
+            />
+          ))
+        )}
+      </svg>
+
+
+    ),
+    title: "Tratamientos",
+    description: "Gestionar tratamientos de los pacientes",
+    link: "/tratamientos",
+    button: "Explorar",
+  },
+
+  addtratamiento: {
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1}
+        stroke="currentColor"
+        className="size-24"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+        />
+      </svg>
+    ),
+    title: "Crear tratamiento",
+    description: "Crear un tratamiento para un paciente",
+    link: "/tratamientos/crear",
+    button: "Crear",
+  },
+
+  editratamiento: {
+    svg: (
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="size-24">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+      </svg>
+
+
+    ),
+    title: "Gestionar tratamientos",
+    description: "Gestionar tratamientos de los pacientes",
+    link: "/tratamientos/listadotratamientos",
+    button: "Gestionar",
+  },
+
+  addregistro: {
+    svg: (
+      <svg 
+        xmlns="http://www.w3.org/2000/svg" 
+        fill="none" 
+        viewBox="0 0 24 24" 
+        stroke-width="1" 
+        stroke="currentColor" 
+        class="size-24"
+      >
+        <path 
+          stroke-linecap="round" 
+          stroke-linejoin="round" 
+          d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75" />
+      </svg>
+
+    ),
+    title: "Registrar cumplimiento",
+    description: "Añadir un registro de cumplimiento para un tratamiento activo",
+    link: "/tratamientos/registro",
+    button: "Registrar",
+  },
+
   default: {
     svg: (
       <svg
