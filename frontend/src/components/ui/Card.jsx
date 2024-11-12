60c2145371d0ab15754eb74f34eef16d40e5969a
Mihnea Vidican <jvidican103@gmail.com>
2024-11-12 13:45:56 +0100

    :sparkles: add: componente acceso a informe de adherencia closes #39

--- frontend/src/components/ui/Card.jsx
+++ frontend/src/components/ui/Card.jsx
@@ -289,17 +289,17 @@ const cardContent = {
 
   addregistro: {
     svg: (
-      <svg 
-        xmlns="http://www.w3.org/2000/svg" 
-        fill="none" 
-        viewBox="0 0 24 24" 
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
         strokeWidth={1}
-        stroke="currentColor" 
+        stroke="currentColor"
         class="size-24"
       >
-        <path 
-          stroke-linecap="round" 
-          stroke-linejoin="round" 
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
           d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75" />
       </svg>
 
@@ -356,17 +356,17 @@ const cardContent = {
 
   registro: {
     svg: (
-      <svg 
-        xmlns="http://www.w3.org/2000/svg" 
-        fill="none" 
-        viewBox="0 0 24 24" 
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
         strokeWidth={1}
-        stroke="currentColor" 
+        stroke="currentColor"
         class="size-24"
       >
-        <path 
-          stroke-linecap="round" 
-          stroke-linejoin="round" 
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
           d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75" />
       </svg>
 
@@ -375,6 +375,27 @@ const cardContent = {
     description: "Añadir un registro de cumplimiento para un tratamiento activo",
     link: "/tratamientos/cumplimiento",
     button: "Añadir",
+  },
+
+  adherencia: {
+    svg: (
+      <svg 
+        xmlns="http://www.w3.org/2000/svg" 
+        fill="none" 
+        viewBox="0 0 24 24" 
+        strokeWidth={1} 
+        stroke="currentColor" 
+        className="size-24">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6a7.5 7.5 0 1 0 7.5 7.5h-7.5V6Z" />
+        <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 10.5H21A7.5 7.5 0 0 0 13.5 3v7.5Z" />
+      </svg>
+
+
+    ),
+    title: "Análisis de adherencia",
+    description: "Consultar mi informe de adherencia terapéutica",
+    link: "/tratamientos/adherencia",
+    button: "Explorar",
   },
 
   default: {
@@ -405,7 +426,7 @@ const Card = ({ title }) => {
   const content = cardContent[title] || cardContent["default"];
 
   return (
-    <div className="card bg-base-100 w-96 h-96 shadow-lg hover:shadow-2xl transition-shadow duration-300 flex flex-col items-center justify-between p-6">
+    <div className="card bg-base-100 w-96 h-96 shadow-lg hover:shadow-2xl transition-shadow duration-300 items-center justify-between p-6">
       <figure className="flex justify-center">{content.svg}</figure>
       <div className="card-body items-center text-center">
         <h2 className="card-title text-xl font-bold">{content.title}</h2>
